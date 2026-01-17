451848a0fb0e3966fcfb893c9ee590f31816d359
Pauputchi78 <pauetxp@gmail.com>
2026-01-17 12:16:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/hotel_pere_maria_app/ui/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.hotel_pere_maria_app.ui
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+
+        }
+    }
+}
