87f36abf512e3da4a5e26660adf664cf9e2f94e0
Paulinux <pauetxp@gmail.com>
2026-02-13 10:08:39 +0100

    Login Basico Con Reenvio de token implementado

--- app/src/main/java/com/example/hotel_pere_maria_app/ui/Service/RetrofitClient.kt
+++ app/src/main/java/com/example/hotel_pere_maria_app/ui/Service/RetrofitClient.kt
@@ -1,12 +1,30 @@
 package com.example.hotel_pere_maria_app.ui.Service
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
     private const val BASE_URL = "http://192.168.1.108:3000/"
+
+    private val authInterceptor = Interceptor{ chain ->
+        val request = chain.request().newBuilder()
+
+        SessionManager.userToken?.let {
+            request.addHeader("Authorization", "Bearer ${it}")
+        }
+        chain.proceed(request.build())
+    }
+    private val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(authInterceptor).addInterceptor(HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        })
+        .build()
     private val retrofit : Retrofit by lazy {
         Retrofit.Builder().baseUrl(BASE_URL)
+            .client(okHttpClient)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
